64ad0bb30559428e4a1936ed348ffe816213fda7
kevinMartinezC <ceronk003@gmail.com>
2023-05-11 07:28:10 -0600

    feat: show items in a vertical grind with 3 columns

--- app/src/main/java/com/example/animeapp/components/search/SearchScreen.kt
+++ app/src/main/java/com/example/animeapp/components/search/SearchScreen.kt
@@ -3,11 +3,14 @@ package com.example.animeapp.components.search
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -20,9 +23,13 @@ import com.example.domain.Anime
 @Composable
 fun SearchScreen(animeList: List<Anime>, modifier: Modifier = Modifier) {
     Box(modifier = modifier) {
-        LazyColumn(modifier = modifier) {
-            items(animeList) { anime ->
-                AnimeCard(anime = anime, modifier = Modifier.padding(8.dp))
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(3),
+            contentPadding = PaddingValues(8.dp),
+            modifier = modifier
+        ) {
+            items(animeList.size) { index ->
+                AnimeCard(anime = animeList[index], modifier = Modifier.padding(4.dp))
             }
         }
     }
@@ -30,21 +37,30 @@ fun SearchScreen(animeList: List<Anime>, modifier: Modifier = Modifier) {
 
 @Composable
 fun AnimeCard(anime: Anime, modifier: Modifier = Modifier) {
-    Card(modifier = modifier, shape = RoundedCornerShape(4.dp)) {
-        Column {
-            anime.imageUrl?.let { url ->
-                NetworkImage(
-                    url = url,
-                    contentDescription = anime.title,
-                    modifier = Modifier
-                        .height(200.dp)
-                        .fillMaxWidth()
-                )
-            }
-            Text(text = anime.title, modifier = Modifier.padding(8.dp))
+    Card(
+        modifier = modifier
+            .size(width = 150.dp, height = 270.dp)
+            .padding(4.dp),
+        shape = RoundedCornerShape(4.dp)
+    ) {
+        Column(modifier = Modifier.fillMaxSize()) {
+            NetworkImage(
+                url = anime.imageUrl,
+                contentDescription = anime.title,
+                modifier = Modifier
+                    .height(180.dp)
+                    .fillMaxWidth()
+            )
+            Text(
+                text = anime.title,
+                modifier = Modifier.padding(2.dp),
+                maxLines = 3,
+            )
         }
     }
 }
+
+
 
 @Composable
 fun NetworkImage(url: String, contentDescription: String, modifier: Modifier = Modifier) {
