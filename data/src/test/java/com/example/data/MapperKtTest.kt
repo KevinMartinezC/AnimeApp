e8e5fbe2cc74023a790bdd57f866aead69f77c09
KevinMartinezC <90873838+KevinMartinezC@users.noreply.github.com>
2023-05-27 18:42:15 -0600

    feat: add UnitTest to verify Mappers are working well

---
+++ data/src/test/java/com/example/data/MapperKtTest.kt
@@ -0,0 +1,46 @@
+package com.example.data
+
+import com.example.data.local.FavoriteAnimeEntity
+import com.example.data.mapper.room.toFavoriteAnime
+import com.example.data.mapper.room.toFavoriteAnimeEntity
+import com.example.domain.model.favorite.FavoriteAnime
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class MapperKtTest {
+    @Test
+    fun `verify toFavoriteAnime maps correctly`() {
+        // Create a FavoriteAnimeEntity
+        val favoriteAnimeEntity = FavoriteAnimeEntity(
+            id = 1,
+            title = "Test Anime",
+            coverImage = "Test URL"
+        )
+
+        // Convert to FavoriteAnime
+        val favoriteAnime = favoriteAnimeEntity.toFavoriteAnime()
+
+        // Verify fields are the same
+        assertEquals(favoriteAnimeEntity.id, favoriteAnime.id)
+        assertEquals(favoriteAnimeEntity.title, favoriteAnime.title)
+        assertEquals(favoriteAnimeEntity.coverImage, favoriteAnime.imageUrl)
+    }
+
+    @Test
+    fun `verify toFavoriteAnimeEntity maps correctly`() {
+
+        val favoriteAnime = FavoriteAnime(
+            id = 1,
+            title = "Test Anime",
+            imageUrl = "Test URL"
+        )
+
+        // Convert to FavoriteAnimeEntity
+        val favoriteAnimeEntity = favoriteAnime.toFavoriteAnimeEntity()
+
+        // Verify fields are the same
+        assertEquals(favoriteAnime.id, favoriteAnimeEntity.id)
+        assertEquals(favoriteAnime.title, favoriteAnimeEntity.title)
+        assertEquals(favoriteAnime.imageUrl, favoriteAnimeEntity.coverImage)
+    }
+}
